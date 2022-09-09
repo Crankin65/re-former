c88c30a80ea5d5f24cfc6ef4a6a40149463eee57
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2022-09-09 11:30:42 -0400

    Created edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-   @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-  # @user = User.new(user_params)
+  #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
       #flash[:success] = "Great! Your post has been created!"
@@ -15,9 +16,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
   
   def user_params
-    params.require(:user).permit(:username,:email,:password)
+    params.require(:user).permit(:id, :username,:email,:password)
   end
 end
