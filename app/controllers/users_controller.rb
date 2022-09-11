feeb6ca29a52ecda8ab2c276e92a9b65dec225bf
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2022-09-11 14:38:45 -0400

    Minor updates, final build

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
+   # binding.pry
   #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
@@ -12,7 +13,7 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       #flash.now[:error] = "Rats! Fix your mistakes, please."
-      render :new, status: :unprocessable_entity
+      render :new, status: 422
     end
   end
 
@@ -20,9 +21,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit, status: 422
+    end
+  end
+
   private
   
   def user_params
-    params.require(:user).permit(:id, :username,:email,:password)
+    params.require(:user).permit(:username,:email,:password)
   end
 end
